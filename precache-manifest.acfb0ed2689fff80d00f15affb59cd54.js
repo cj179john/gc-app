92f402a4f68a00b6a92d61ba7a86f631d23aaeef
John Jiang <john.jiang@checkit.net>
2020-07-04 08:30:09 +0100

    Updates

---
+++ precache-manifest.acfb0ed2689fff80d00f15affb59cd54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a6fda75dbc8edfeeefcad57d6616a97",
+    "url": "/gc-app/index.html"
+  },
+  {
+    "revision": "774793cd77981b226020",
+    "url": "/gc-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a2221d600386a9761a73",
+    "url": "/gc-app/static/js/2.bbdec4c9.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/gc-app/static/js/2.bbdec4c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "774793cd77981b226020",
+    "url": "/gc-app/static/js/main.0ae3eef4.chunk.js"
+  },
+  {
+    "revision": "0c511bb7771a174d1cb7",
+    "url": "/gc-app/static/js/runtime-main.a8103b72.js"
+  }
+]);
